a5538d5f063042223d76f4ef05676f03dac09952
Dennis Baldwin <dennisbaldwin@gmail.com>
2018-05-30 11:30:11 -0500

    Change wording

--- blockly/blocks/droneblocks/toggle_leds.js
+++ blockly/blocks/droneblocks/toggle_leds.js
@@ -1,7 +1,7 @@
 Blockly.Blocks['toggle_leds'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("turn front LEDs ")
+        .appendField("turn LEDs ")
         .appendField(new Blockly.FieldDropdown([["on", "on"], ["off", "off"]]), "led_status");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
